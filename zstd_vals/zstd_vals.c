d62a2eab05933fcf7e433f9a2ad3ecac598ffe59
Nick Chadwick <chadnickbok@gmail.com>
2016-11-11 11:04:51 -0800

    Add ZSETLEVEL

--- zstd_vals/zstd_vals.c
+++ zstd_vals/zstd_vals.c
@@ -6,51 +6,6 @@
 #include <zstd.h>
 
 /*
-* example.HGETSET <key> <element> <value>
-* Atomically set a value in a HASH key to <value> and return its value before
-* the HSET.
-*
-* Basically atomic HGET + HSET
-*/
-int HGetSetCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-
-  // we need EXACTLY 4 arguments
-  if (argc != 4) {
-    return RedisModule_WrongArity(ctx);
-  }
-  RedisModule_AutoMemory(ctx);
-
-  // open the key and make sure it's indeed a HASH and not empty
-  RedisModuleKey *key =
-      RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
-  if (RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_HASH &&
-      RedisModule_KeyType(key) != REDISMODULE_KEYTYPE_EMPTY) {
-    return RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
-  }
-
-  // get the current value of the hash element
-  RedisModuleCallReply *rep =
-      RedisModule_Call(ctx, "HGET", "ss", argv[1], argv[2]);
-  RMUTIL_ASSERT_NOERROR(rep);
-
-  // set the new value of the element
-  RedisModuleCallReply *srep =
-      RedisModule_Call(ctx, "HSET", "sss", argv[1], argv[2], argv[3]);
-  RMUTIL_ASSERT_NOERROR(srep);
-
-  // if the value was null before - we just return null
-  if (RedisModule_CallReplyType(rep) == REDISMODULE_REPLY_NULL) {
-    RedisModule_ReplyWithNull(ctx);
-    return REDISMODULE_OK;
-  }
-
-  // forward the HGET reply to the client
-  RedisModule_ReplyWithCallReply(ctx, rep);
-  return REDISMODULE_OK;
-}
-
-
-/*
  * zstd_vals.ZSET <key> <value>
  * Compress and store a key.
  */
@@ -81,6 +36,60 @@ int ZSETCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     // XXX: Do I need to check return value of RedisModule_Alloc?
 
     size_t res = ZSTD_compress(buf, bound, value, len, 1); // Default super-fast mode
+    if (ZSTD_isError(res))
+    {
+        RedisModule_CloseKey(key);
+        const char *zstd_err = ZSTD_getErrorName(res);
+        return RedisModule_ReplyWithError(ctx, zstd_err);
+    }
+
+    RedisModuleString *compressed_string = RedisModule_CreateString(ctx, (const char *)buf, res);
+    
+    RedisModule_StringSet(key, compressed_string);
+    RedisModule_CloseKey(key);
+    RedisModule_Free(buf);
+
+    RedisModule_ReplyWithSimpleString(ctx,"OK");    
+    return REDISMODULE_OK;
+}
+
+/**
+ * zstd_vals.ZSETLEVEL <key> <level> <value>
+ */
+int ZSETLEVELCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    if (argc != 4) 
+    {
+        return RedisModule_WrongArity(ctx);
+    }
+    RedisModule_AutoMemory(ctx);
+
+    RedisModuleKey *key = RedisModule_OpenKey(ctx,argv[1], REDISMODULE_READ|REDISMODULE_WRITE);
+    int keytype = RedisModule_KeyType(key);
+    if ((keytype != REDISMODULE_KEYTYPE_STRING) && (keytype != REDISMODULE_KEYTYPE_EMPTY))
+    {
+        RedisModule_CloseKey(key);
+        return RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
+    }
+
+    long long level_arg;
+    if (RedisModule_StringToLongLong(argv[2], &level_arg) != REDISMODULE_OK)
+    {
+        RedisModule_CloseKey(key);
+        RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
+    }
+
+    int level = (int) level_arg;
+
+    size_t len;
+    const char *value = RedisModule_StringPtrLen(argv[3], &len);
+    // XXX: Do I need to check for errors here? ie. zero-length values
+
+    size_t bound = ZSTD_compressBound(len);
+    void *buf = RedisModule_Alloc(bound);
+    // XXX: Do I need to check return value of RedisModule_Alloc?
+
+    size_t res = ZSTD_compress(buf, bound, value, len, level); // Default super-fast mode
     if (ZSTD_isError(res))
     {
         RedisModule_CloseKey(key);
@@ -135,52 +144,6 @@ int ZGETCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     return REDISMODULE_OK;
 }
 
-/**
- * zstd_vals.ZCHECK <key>
- * Confirm the given key is a valid ZSTD-compressed blob.
- */
-int ZCHECKCommand() {
-    return REDISMODULE_OK;
-}
-
-// Test the the PARSE command
-int testParse(RedisModuleCtx *ctx) {
-
-  RedisModuleCallReply *r =
-      RedisModule_Call(ctx, "example.parse", "ccc", "SUM", "5", "2");
-  RMUtil_Assert(RedisModule_CallReplyType(r) == REDISMODULE_REPLY_INTEGER);
-  RMUtil_AssertReplyEquals(r, "7");
-
-  r = RedisModule_Call(ctx, "example.parse", "ccc", "PROD", "5", "2");
-  RMUtil_Assert(RedisModule_CallReplyType(r) == REDISMODULE_REPLY_INTEGER);
-  RMUtil_AssertReplyEquals(r, "10");
-  return 0;
-}
-
-// test the HGETSET command
-int testHgetSet(RedisModuleCtx *ctx) {
-  RedisModuleCallReply *r =
-      RedisModule_Call(ctx, "example.hgetset", "ccc", "foo", "bar", "baz");
-  RMUtil_Assert(RedisModule_CallReplyType(r) != REDISMODULE_REPLY_ERROR);
-
-  r = RedisModule_Call(ctx, "example.hgetset", "ccc", "foo", "bar", "bag");
-  RMUtil_Assert(RedisModule_CallReplyType(r) == REDISMODULE_REPLY_STRING);
-  RMUtil_AssertReplyEquals(r, "baz");
-  r = RedisModule_Call(ctx, "example.hgetset", "ccc", "foo", "bar", "bang");
-  RMUtil_AssertReplyEquals(r, "bag");
-  return 0;
-}
-
-// Unit test entry point for the module
-int TestModule(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  RedisModule_AutoMemory(ctx);
-
-  RMUtil_Test(testParse);
-  RMUtil_Test(testHgetSet);
-
-  RedisModule_ReplyWithSimpleString(ctx, "PASS");
-  return REDISMODULE_OK;
-}
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx) {
     // Register the module itself
@@ -188,9 +151,19 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx) {
         return REDISMODULE_ERR;
     }
 
+    // Basic commands
     RMUtil_RegisterWriteCmd(ctx, "zstd.ZSET", ZSETCommand);
+    // zstd.ZSETLEVEL <key> <level> <string> - compress using the given level
     RMUtil_RegisterReadCmd(ctx, "zstd.ZGET", ZGETCommand);
-    // RMUtil_RegisterReadCmd(ctx, "zstd.ZCHECK", ZCHECKCommand);
+
+    // Compress with level
+    RMUtil_RegisterWriteCmd(ctx, "zstd.ZSETLEVEL", ZSETLEVELCommand);
+
+    // Scary commands that can lose data (if a dict is lost so are all its compressed values)
+    // zstd.ZDICT <key> <string> - upload a new compression dictionary with the given key
+    // zstd.ZDICTSET <key> <dictkey> <string> - compress using the given dictionary
+    // zstd.ZDICTSETLEVEL <key> <dictkey> <level> <string> - compress using the given dict and level
+    // zstd.ZDICTGET <key> <dictkey> - get the value stored at key decompressed with the dictionary at dictkey
 
     return REDISMODULE_OK;
 }
